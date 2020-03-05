158168b7acbdedacb94918a42dd4e5dc12e10d0c
Maicol Rojas <maicolandreyrojas@gmail.com>
2020-03-05 10:21:55 -0500

    Borrando usuarios

--- vistas/js/usuarios.js
+++ vistas/js/usuarios.js
@@ -186,3 +186,37 @@ $("#nuevoUsuario").change(function(){
 });
 
 /*=====  End of REVISAR USUARIO SI YA ESTA REGISTRADO  ======*/
+
+/*======================================
+=            BORRAR USUARIO            =
+======================================*/
+
+$(".btnEliminarUsuario").click(function(){
+
+  var idUsuario = $(this).attr("idUsuario");
+  var fotoUsuario = $(this).attr("fotoUsuario");
+  var usuario = $(this).attr("usuario");
+
+  swal({
+    title: '¿Está seguro de borrar el usuario?',
+    text: "¡Si no lo está puede cancelar la accíón!",
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Si, borrar usuario!'
+  }).then(function(result){
+
+  	
+    if(result.value){
+
+      window.location = "index.php?ruta=usuarios&idUsuario="+idUsuario+"&usuario="+usuario+"&fotoUsuario="+fotoUsuario;
+
+    }
+
+  })
+
+})
+
+/*=====  End of BORRAR USUARIO  ======*/
